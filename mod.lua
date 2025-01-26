601fc6b434bf2e6a00458fe8f76b272db9483841
hyperboid <23138363+polypoyo@users.noreply.github.com>
2025-01-26 02:42:34 -0500

    More advanced runGame method

--- mod.lua
+++ mod.lua
@@ -18,7 +18,9 @@ function Mod:postInit()
     self:runGame()
 end
 
-function Mod:runGame()
+function Mod:runGame(game, quit_callback)
+    self.game = game or self.game
+    quit_callback = quit_callback or Kristal.returnToMenu
     local main_chunk = love.filesystem.load(self:getGamePath().."/main.lua")
     assert(main_chunk, "Missing main.lua ("..self:getGamePath()..")")
     setfenv(main_chunk, GameEnv)
@@ -38,13 +40,14 @@ function Mod:runGame()
         love.graphics.present()
         if result ~= nil then
             if result == 0 or result == "restart" then
-                Kristal.returnToMenu()
-                return
+                break
             else
                 error("Game exited abnormally! Status code "..result)
             end
         end
     end
+    quit_callback()
+    self:cleanup()
 end
 
 -- function Mod:postUpdate()
@@ -56,6 +59,10 @@ end
 -- end
 
 function Mod:unload()
+    self:cleanup()
+end
+
+function Mod:cleanup()
     love.filesystem.unmount(self:getGamePath())
     Kristal.resetWindow()
     Kristal.setVolume(Kristal.Config["masterVolume"] or 0.6)
