9056da3dcc5b20df23fcacf366a6d0bdf22c014d
hyperboid <23138363+polypoyo@users.noreply.github.com>
2025-01-26 01:48:32 -0500

    Better "trivial"

--- games/trivial/main.lua
+++ games/trivial/main.lua
@@ -1,4 +1,15 @@
 function love.draw()
     love.graphics.setColor(1,0,1)
     love.graphics.circle("fill", 50, 50, 100)
+    love.graphics.setColor(1,1,1)
+    love.graphics.print("Press ESC to quit.")
+    if OrigGlobal and OrigGlobal.Kristal then
+        love.graphics.print("...and return to Kristal.", 0, 32)
+    end
+end
+
+function love.keypressed(key)
+    if key == "escape" then
+        love.event.quit()
+    end
 end
