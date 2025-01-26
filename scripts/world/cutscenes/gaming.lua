9bf10e95d887d100019615362bac8012230db601
hyperboid <23138363+polypoyo@users.noreply.github.com>
2025-01-26 03:41:55 -0500

    Fix choicer dumb

--- scripts/world/cutscenes/gaming.lua
+++ scripts/world/cutscenes/gaming.lua
@@ -2,7 +2,7 @@
 ---@param game string
 return function (cutscene, game)
     cutscene:text("* It's an arcade cabinet for \""..game:upper()..".\"\n* Play it?")
-    if cutscene:choicer({"Play", "Don't"}) then
+    if cutscene:choicer({"Play", "Don't"}) == 1 then
         cutscene:wait(cutscene:fadeOut(2))
         Mod:runGame(game, function()end)
         cutscene:wait(cutscene:fadeIn(2))
