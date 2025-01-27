96edd24e527acd87a56b5f167e3538cc5988f57f
hyperboid <23138363+polypoyo@users.noreply.github.com>
2025-01-27 03:08:27 -0500

    Overengineered icon detection

--- scripts/world/events/cab.lua
+++ scripts/world/events/cab.lua
@@ -9,6 +9,8 @@ function cab:init(data, ...)
     else
         self:setSprite("events/cab/generic")
     end
+    self:scanForIcon("icon.png")
+    self:scanForIcon("graphics/icon.png")
     self:setHitbox(0,0,self:getSize())
     self.solid = true
     if not Mod:hasGame(self.game) then
@@ -16,9 +18,29 @@ function cab:init(data, ...)
     end
 end
 
+function cab:scanForIcon(path)
+    if self.icon then return end
+    if love.filesystem.getInfo(Mod:getGamePath(self.game).."/"..path) then
+        local icon = love.graphics.newImage(Mod:getGamePath(self.game).."/"..path)
+        local prev_canvas = love.graphics.getCanvas()
+        local canvas = Draw.pushCanvas(16,16)
+        Draw.draw(icon, 0,0, 0, 16/icon:getWidth(), 16/icon:getWidth())
+        Draw.popCanvas()
+        self.icon = love.graphics.newImage(canvas:newImageData())
+        love.graphics.setCanvas(prev_canvas)
+    end
+end
+
 function cab:onAdd(parent)
     if not self.game then self:remove() end
     super.onAdd(self,parent)
+end
+
+function cab:draw()
+    super.draw(self)
+    if self.icon then
+        Draw.draw(self.icon, 30,27, 0, 2,2)
+    end
 end
 
 function cab:onInteract()
